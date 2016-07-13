1208848be2ecfe02110b8aa60ae5a53a6d52c771
Timothy Wood <codearachnid@gmail.com>
2016-07-13 16:37:20 -0400

    Create pitchpro_plugin.php

---
+++ pitchpro_plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Pitch Pro
+Description: The best tool for creating pitches
+Version: 0.0.1
+Author: Imagine Simplicity
+Author URI: http://www.imaginesimplicity.com
+Text Domain: pitchpro
+License: GPLv3 or later
+*/
+/*
+Copyright 2009-2016 by Pitch Pro, Imagine Simplicity and 
+additional contributors This program is free software; you can 
+redistribute it and/or modify it under the terms of the GNU 
+General Public License as published by the Free Software 
+Foundation; either version 3 of the License, or (at your option) 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
